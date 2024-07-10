c84dee2ac5c74c4d71407976bc6d8cd102079f35
jo <q1787823054@gmail.com>
2024-07-10 08:48:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,9 +12,9 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://d1bbb57c-c1c1-441b-8720-d7b20a67af39@162.159.60.57:443?encryption=none&security=tls&sni=free1.makabahay.sbs&fp=randomized&type=ws&host=free1.makabahay.sbs&path=%2F%3Fed%3D2560#%E5%90%8E%E7%BC%80%E5%B8%A6%E6%97%B6%E9%97%B4%E7%9A%84%E5%B0%91%E7%94%A8
-vless://d1bbb57c-c1c1-441b-8720-d7b20a67af39@162.159.60.57:443?encryption=none&security=tls&sni=free1.makabahay.sbs&fp=randomized&type=ws&host=free1.makabahay.sbs&path=%2F%3Fed%3D2560#CF%E4%BC%98%E9%80%89%2025.5.31
-vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIkxhbSBVUyBWTUVTUyAyNS4yLjEiLA0KICAiYWRkIjogIjE2Mi4xNTkuNjAuNTciLA0KICAicG9ydCI6ICIyMDUzIiwNCiAgImlkIjogImE2YWRkN2U1LWNjMDMtNDU5NS1hYTgxLWE2MzMzMjdjOWVlMyIsDQogICJhaWQiOiAiMCIsDQogICJzY3kiOiAiYXV0byIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAibGFtLm1ha2FiYWhheS5zYnMiLA0KICAicGF0aCI6ICJhNmFkZDdlNS1jYzAzLTQ1OTUtYWE4MS1hNjMzMzI3YzllZTMtdm0iLA0KICAidGxzIjogInRscyIsDQogICJzbmkiOiAibGFtLm1ha2FiYWhheS5zYnMiLA0KICAiYWxwbiI6ICIiLA0KICAiZnAiOiAiIg0KfQ==
+vless://d1bbb57c-c1c1-441b-8720-d7b20a67af39@185.146.173.182:443?encryption=none&security=tls&sni=free1.makabahay.sbs&fp=randomized&type=ws&host=free1.makabahay.sbs&path=%2F%3Fed%3D2560#%E5%90%8E%E7%BC%80%E5%B8%A6%E6%97%B6%E9%97%B4%E7%9A%84%E5%B0%91%E7%94%A8
+vless://d1bbb57c-c1c1-441b-8720-d7b20a67af39@185.146.173.182:443?encryption=none&security=tls&sni=free1.makabahay.sbs&fp=randomized&type=ws&host=free1.makabahay.sbs&path=%2F%3Fed%3D2560#CF%E4%BC%98%E9%80%89%2025.5.31
+vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIkxhbSBVUyBWTUVTUyAyNS4yLjEiLA0KICAiYWRkIjogIjE4NS4xNDYuMTczLjE4MiIsDQogICJwb3J0IjogIjIwNTMiLA0KICAiaWQiOiAiYTZhZGQ3ZTUtY2MwMy00NTk1LWFhODEtYTYzMzMyN2M5ZWUzIiwNCiAgImFpZCI6ICIwIiwNCiAgInNjeSI6ICJhdXRvIiwNCiAgIm5ldCI6ICJ3cyIsDQogICJ0eXBlIjogIm5vbmUiLA0KICAiaG9zdCI6ICJsYW0ubWFrYWJhaGF5LnNicyIsDQogICJwYXRoIjogImE2YWRkN2U1LWNjMDMtNDU5NS1hYTgxLWE2MzMzMjdjOWVlMy12bSIsDQogICJ0bHMiOiAidGxzIiwNCiAgInNuaSI6ICJsYW0ubWFrYWJhaGF5LnNicyIsDQogICJhbHBuIjogIiIsDQogICJmcCI6ICIiDQp9
 hysteria2://a6add7e5-cc03-4595-aa81-a633327c9ee3@lam.makabahay.sbs:37531/?sni=lam.makabahay.sbs&alpn=h3&insecure=0#Lam%20US%20HY2%2025.2.1
 hysteria2://6FStGEw3Xbb98wQv08JPrMaH@[2604:abc0:103:415:2::cf]:40443/?sni=bing.com&insecure=1#PAS%20US%2024.7.25
 hysteria2://fa3b25cb-e53b-4e62-88ca-e7c18fd3bafa@gcp.makabahay.sbs:17096/?sni=gcp.makabahay.sbs&alpn=h3&insecure=0#GCP%20JP%2024.9.2
